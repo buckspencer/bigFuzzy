4213b22b75065950f9ce411b91b3056c23cb570c
Buck Spencer <buck.spencer@mutations.ltd>
2023-05-03 12:52:37 -0700

    Working API call with form

--- pages/api/replicateCall.js
+++ pages/api/replicateCall.js
@@ -1,8 +1,9 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 import Replicate from "replicate";
 
 export default async function replicateCall(req, res) {
+	const queryRegex = /\?([^#]+)/;
+	const parsedPrompt = decodeURIComponent(queryRegex.exec(req.url));
+
 	const replicate = new Replicate({
 		auth: process.env.REPLICATE_API_TOKEN,
 	});
@@ -10,7 +11,7 @@ export default async function replicateCall(req, res) {
 		"ai-forever/kandinsky-2:601eea49d49003e6ea75a11527209c4f510a93e2112c969d548fbb45b9c4f19f",
 		{
 			input: {
-				prompt: "red otter wearing royal cloths, 4k photo",
+				parsedPrompt,
 			},
 		}
 	);
