891544328ffdf41b7e89ae34a96bb96a69090ea4
Buck Spencer <buck.spencer@mutations.ltd>
2023-05-12 13:47:10 -0700

    Setup Sanity with user

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,15 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import Navbar from "../components/Navbar";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <GoogleOAuthProvider
+	return <Component {...pageProps} />;
+}
+{
+	/* <GoogleOAuthProvider
       clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}
     >
       <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
         <Navbar />
         <Component {...pageProps} />
       </div>
-    </GoogleOAuthProvider>
-  );
+    </GoogleOAuthProvider> */
 }
