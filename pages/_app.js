dda5b5084f5ea2f417dd0a2699ad2efe3da97eb5
Buck Spencer <buck.spencer@mutations.ltd>
2023-05-12 09:57:59 -0700

    WIP:Adding Navbar login

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return;
+  <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
+    <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
+      <div className="flex gap-6 md:gap-20">
+        <Navbar />
+      </div>
+      <Component {...pageProps} />
+    </div>
+  </GoogleOAuthProvider>;
 }
