872e40e2acdceb9eb2f98eb7d1f5115d2cf39bf8
Buck Spencer <buck.spencer@mutations.ltd>
2023-05-22 12:33:02 -0700

    Update filename

---
+++ pages/api/replicateImageCall.js
@@ -0,0 +1,19 @@
+import Replicate from "replicate";
+
+export default async function replicateImageCall(req, res) {
+	const queryRegex = /\?([^#]+)/;
+	const parsedPrompt = queryRegex.exec(req.url);
+	const decodedPrompt = decodeURIComponent(parsedPrompt[1]);
+	const replicate = new Replicate({
+		auth: process.env.NEXT_REPLICATE_API_TOKEN,
+	});
+	const output = await replicate.run(
+		"ai-forever/kandinsky-2:601eea49d49003e6ea75a11527209c4f510a93e2112c969d548fbb45b9c4f19f",
+		{
+			input: {
+				prompt: decodedPrompt,
+			},
+		}
+	);
+	res.status(200).json(output);
+}
