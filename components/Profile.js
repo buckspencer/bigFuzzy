3427f46ada27d68f8fafe88f981a8b5d497a6dcd
Buck Spencer <buck.spencer@mutations.ltd>
2023-05-22 12:33:19 -0700

    Add build screen

---
+++ components/Profile.js
@@ -0,0 +1,285 @@
+import {
+	CheckIcon,
+	QuestionMarkCircleIcon,
+	StarIcon,
+} from "@heroicons/react/20/solid";
+import React, { useState } from "react";
+
+import Image from "next/image";
+import { RadioGroup } from "@headlessui/react";
+import { ShieldCheckIcon } from "@heroicons/react/24/outline";
+import petImage from "../assets/pet_image.jpg";
+
+const product = {
+	name: "Enter the details for your pet's portrait",
+	href: "#",
+	price: "$20",
+	description:
+		"Big Fuzzy will send you a handmade portrait of your marvelous creation.",
+	imageSrc:
+		"https://tailwindui.com/img/ecommerce-images/product-page-04-featured-product-shot.jpg",
+	imageAlt:
+		"Model wearing light green backpack with black canvas straps and front zipper pouch.",
+	sizes: [
+		{ name: "18L", description: "Perfect for a reasonable amount of snacks." },
+		{ name: "20L", description: "Enough room for a serious amount of snacks." },
+	],
+};
+const reviews = { average: 4, totalCount: 1624 };
+
+function classNames(...classes) {
+	return classes.filter(Boolean).join(" ");
+}
+
+const Profile = () => {
+	const [selectedSize, setSelectedSize] = useState(product.sizes[0]);
+
+	return (
+		<div className="bg-white">
+			<div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+				{/* Product details */}
+				<div className="lg:max-w-lg lg:self-end">
+					<div className="mt-4">
+						<h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+							{product.name}
+						</h1>
+					</div>
+
+					<section aria-labelledby="information-heading" className="mt-4">
+						<h2 id="information-heading" className="sr-only">
+							Product information
+						</h2>
+
+						<div className="flex items-center">
+							<p className="text-lg text-gray-900 sm:text-xl">
+								{product.price}
+							</p>
+
+							<div className="ml-4 border-l border-gray-300 pl-4">
+								<h2 className="sr-only">Reviews</h2>
+								<div className="flex items-center">
+									<div>
+										<div className="flex items-center">
+											{[0, 1, 2, 3, 4].map((rating) => (
+												<StarIcon
+													key={rating}
+													className={classNames(
+														reviews.average > rating
+															? "text-yellow-400"
+															: "text-gray-300",
+														"h-5 w-5 flex-shrink-0"
+													)}
+													aria-hidden="true"
+												/>
+											))}
+										</div>
+										<p className="sr-only">{reviews.average} out of 5 stars</p>
+									</div>
+									<p className="ml-2 text-sm text-gray-500">
+										{reviews.totalCount} reviews
+									</p>
+								</div>
+							</div>
+						</div>
+
+						<div className="mt-4 space-y-6">
+							<p className="text-base text-gray-500">{product.description}</p>
+						</div>
+
+						<div class="flex">
+							<div className="flex  mt-5">
+								<label
+									htmlFor="last-name"
+									className="block text-sm font-medium leading-6 text-black"
+								>
+									First name
+								</label>
+								<div className="mt-2 ml-2">
+									<input
+										type="text"
+										name="first-name"
+										id="first-name"
+										autoComplete="given-name"
+										className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-black shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+									/>
+								</div>
+							</div>
+
+							<div className="flex mt-5 ml-2 ">
+								<label
+									htmlFor="last-name"
+									className="block text-sm font-medium leading-6 text-black"
+								>
+									Last name
+								</label>
+								<div className="mt-2 ml-2">
+									<input
+										type="text"
+										name="last-name"
+										id="last-name"
+										autoComplete="family-name"
+										className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-black shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+									/>
+								</div>
+							</div>
+						</div>
+
+						<div className="flex w-full mt-4">
+							<label
+								htmlFor="email"
+								className="block text-sm font-medium leading-6 text-black"
+							>
+								Origin
+							</label>
+						</div>
+						<div className="flex mt-2">
+							<label
+								htmlFor="city"
+								className="block text-sm font-medium leading-6 text-gray-900"
+							>
+								City/State
+							</label>
+							<div className="mt-2 ml-2 mr-2">
+								<input
+									type="text"
+									name="city"
+									id="city"
+									autoComplete="address-level2"
+									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+								/>
+							</div>
+
+							<label
+								htmlFor="email"
+								className="block text-sm font-medium leading-6 text-black"
+							>
+								Country
+							</label>
+							<div className="mt-2 ml-2 w-1/3">
+								<select
+									id="country"
+									name="country"
+									autoComplete="country-name"
+									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+								>
+									<option>United States</option>
+									<option>Canada</option>
+									<option>Mexico</option>
+								</select>
+							</div>
+						</div>
+					</section>
+				</div>
+
+				{/* Product image */}
+				<div className="mt-10 lg:col-start-2 lg:row-span-2 lg:mt-0 lg:self-center">
+					<div className="aspect-h-1 aspect-w-1 overflow-hidden rounded-lg">
+						<Image
+							src={petImage}
+							alt="Generated Pet Image"
+							className="h-full w-full object-cover object-center"
+							width={500}
+							height={500}
+							priority
+						/>
+					</div>
+				</div>
+
+				{/* Product form */}
+				<div className="mt-10 lg:col-start-1 lg:row-start-2 lg:max-w-lg lg:self-start">
+					<section aria-labelledby="options-heading">
+						<h2 id="options-heading" className="sr-only">
+							Product options
+						</h2>
+
+						<form>
+							<div className="sm:flex sm:justify-between">
+								{/* Size selector */}
+								<RadioGroup value={selectedSize} onChange={setSelectedSize}>
+									<RadioGroup.Label className="block text-sm font-medium text-gray-700">
+										Size
+									</RadioGroup.Label>
+									<div className="mt-1 grid grid-cols-1 gap-4 sm:grid-cols-2">
+										{product.sizes.map((size) => (
+											<RadioGroup.Option
+												as="div"
+												key={size.name}
+												value={size}
+												className={({ active }) =>
+													classNames(
+														active ? "ring-2 ring-indigo-500" : "",
+														"relative block cursor-pointer rounded-lg border border-gray-300 p-4 focus:outline-none"
+													)
+												}
+											>
+												{({ active, checked }) => (
+													<>
+														<RadioGroup.Label
+															as="p"
+															className="text-base font-medium text-gray-900"
+														>
+															{size.name}
+														</RadioGroup.Label>
+														<RadioGroup.Description
+															as="p"
+															className="mt-1 text-sm text-gray-500"
+														>
+															{size.description}
+														</RadioGroup.Description>
+														<div
+															className={classNames(
+																active ? "border" : "border-2",
+																checked
+																	? "border-indigo-500"
+																	: "border-transparent",
+																"pointer-events-none absolute -inset-px rounded-lg"
+															)}
+															aria-hidden="true"
+														/>
+													</>
+												)}
+											</RadioGroup.Option>
+										))}
+									</div>
+								</RadioGroup>
+							</div>
+							<div className="mt-4">
+								<a
+									href="#"
+									className="group inline-flex text-sm text-gray-500 hover:text-gray-700"
+								>
+									<span>What size should I buy?</span>
+									<QuestionMarkCircleIcon
+										className="ml-2 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+										aria-hidden="true"
+									/>
+								</a>
+							</div>
+							<div className="mt-10">
+								<button
+									type="submit"
+									className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-black hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
+								>
+									Order Portrait
+								</button>
+							</div>
+							<div className="mt-6 text-center">
+								<a href="#" className="group inline-flex text-base font-medium">
+									<ShieldCheckIcon
+										className="mr-2 h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+										aria-hidden="true"
+									/>
+									<span className="text-gray-500 hover:text-gray-700">
+										Lifetime Guarantee
+									</span>
+								</a>
+							</div>
+						</form>
+					</section>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Profile;
---
+++ pages/build.js
@@ -0,0 +1,8 @@
+import Profile from "../components/Profile";
+import React from "react";
+
+const build = () => {
+  return <Profile />;
+};
+
+export default build;
