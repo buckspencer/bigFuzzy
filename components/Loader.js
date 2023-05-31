e810d4e170b607912c8099cb735b9027529c8ad1
Buck Spencer <buck.spencer@mutations.ltd>
2023-05-31 12:00:16 -0700

    Add cat Loader

--- components/Loader.js
+++ components/Loader.js
@@ -1,9 +1,22 @@
+import Image from "next/image";
+import React from "react";
+import logo from "../assets/cat.svg";
+import styles from "./css/Loader.module.css";
+
 const Loader = () => {
-	return (
-		<div className="grid h-screen place-items-center w-96">
-			<div className="animate-bounce rounded-lg h-32 w-32 border-4 border-slate-640" />
-		</div>
-	);
+  return (
+    <div className="flex items-center justify-center">
+      <div className={styles.container}>
+        <Image
+          src={logo}
+          alt="Cat"
+          width={64}
+          height={64}
+          className={styles.bounce}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Loader;
