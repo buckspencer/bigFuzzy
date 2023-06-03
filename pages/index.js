c9999ff8ee69c049007b99d6238d0aca15aded75
Buck Spencer <buck.spencer@mutations.ltd>
2023-06-03 09:42:19 -0700

    Extract Footer

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 
+import Footer from "@/components/Footer";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Link from "next/link";
@@ -322,41 +323,7 @@ export default function Home() {
 					)}
 				</div>
 			</div>
-
-			<footer className="bg-[#B6D6CC]">
-				<div className="mx-auto max-w-7xl overflow-hidden py-5 px-6 sm:py-24 lg:px-8">
-					<nav
-						className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12"
-						aria-label="Footer"
-					>
-						{navigation.main.map((item) => (
-							<div key={item.name} className="pb-6">
-								<a
-									href={item.href}
-									className="text-sm leading-6 text-gray-600 hover:text-gray-900"
-								>
-									{item.name}
-								</a>
-							</div>
-						))}
-					</nav>
-					<div className="mt-10 flex justify-center space-x-10">
-						{navigation.social.map((item) => (
-							<a
-								key={item.name}
-								href={item.href}
-								className="text-gray-400 hover:text-gray-500"
-							>
-								<span className="sr-only">{item.name}</span>
-								<item.icon className="h-6 w-6" aria-hidden="true" />
-							</a>
-						))}
-					</div>
-					<p className="mt-10 text-center text-xs leading-5 text-gray-500">
-						&copy; 2023 Big Fuzzy, Inc. All rights reserved.
-					</p>
-				</div>
-			</footer>
+			<Footer />
 		</main>
 	);
 }
