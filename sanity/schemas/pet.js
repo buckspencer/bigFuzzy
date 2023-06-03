e5d00ef6e110ffb00f33b3aa070f6ef52c167509
Buck Spencer <buck.spencer@mutations.ltd>
2023-06-03 09:22:20 -0700

    Update pet model with image and imageUrl

--- sanity/schemas/pet.js
+++ sanity/schemas/pet.js
@@ -1,28 +1,36 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  name: "pet",
-  title: "Pet",
-  type: "document",
-  fields: [
-    {
-      name: "createdBy",
-      title: "createdBy",
-      type: "createdBy",
-    },
-    {
-      name: "name",
-      title: "Name",
-      type: "string",
-    },
-    {
-      name: "originStory",
-      title: "OriginStory",
-      type: "string",
-    },
-    {
-      name: "image",
-      title: "Image",
-      type: "string",
-    },
-  ],
+	name: "pet",
+	title: "Pet",
+	type: "document",
+	fields: [
+		{
+			name: "createdBy",
+			title: "createdBy",
+			type: "createdBy",
+		},
+		{
+			name: "name",
+			title: "Name",
+			type: "string",
+		},
+		{
+			name: "originStory",
+			title: "OriginStory",
+			type: "string",
+		},
+		{
+			name: "imageUrl",
+			title: "ImageUrl",
+			type: "string",
+		},
+		{
+			name: "image",
+			title: "Image",
+			type: "file",
+			options: {
+				hotspot: true,
+			},
+		},
+	],
 };
