6c13e60e1de2bf9a7cd0e586a7292c8404c26d2c
Buck Edward Spencer <ad3uck@gmail.com>
2023-06-06 12:02:52 -0700

    Update create pet for more quality

--- pages/api/createPet.js
+++ pages/api/createPet.js
@@ -1,4 +1,5 @@
 import { client } from "../../sanity/lib/client";
+import sharp from "sharp";
 
 export default async function handler(req, res) {
 	if (req.method === "POST") {
@@ -8,19 +9,17 @@ export default async function handler(req, res) {
 		try {
 			// Fetch the image from the provided URL and upload it to Sanity
 			const imageUrl = pet.imageUrl;
-			const imageBuffer = await fetch(imageUrl).then((res) =>
-				res.arrayBuffer()
-			);
-			// console.log("BUFFER");
-			// console.log(imageBuffer);
-			const uploadedImage = await _client.assets.upload(
-				"image",
-				Buffer.from(imageBuffer),
-				{
-					fileName: "namn",
-					contentType: "png",
-				}
-			);
+
+			// Fetch the image from its URL
+			const response = await fetch(imageUrl);
+			// console.log("FETCH");
+			// console.log(response);
+			// Read the image data into a sharp pipeline
+			const pipeline = sharp(await response.arrayBuffer());
+			const uploadedImage = await client.assets.upload("image", pipeline, {
+				fileName: `${pet.name}.jpg`,
+				contentType: "image/jpeg",
+			});
 			// console.log("UploadedImage");
 			// console.log(uploadedImage);
 			// Create the pet object with a reference to the uploaded image
